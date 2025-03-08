225a886e39a4dc8a971d334208ed826777cd00ed
Sazidur Rahman <149562280+saz-idur@users.noreply.github.com>
2025-03-08 22:46:05 +0600

    Delete backend directory

--- backend/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SUPABASE_URL = os.environ.get('SUPABASE_URL')
-    SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')  # For JWT
--- backend/app/routes/__init__.py
+++
@@ -1,1 +0,0 @@
-# Empty file to make routes a package
